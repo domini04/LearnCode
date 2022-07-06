9c705558d826fc6bce59a4251e598488cabc894c
domini04 <107534459+domini04@users.noreply.github.com>
2022-07-06 15:11:42 +0900

    Add files via upload
    Python Dictionary 자료형을 csv 또는 json으로 write하기

---
+++ dict_to_csv.py
@@ -0,0 +1,20 @@
+# dict를 csv로 만들어보기
+
+z = [{'이름':'짱구','수학':90,'영어':100}, {'이름':'짱아','수학':95,'영어':97}]
+import csv
+with open('practice.csv', 'w') as csv_file:
+  fields = ['이름', '수학','영어']
+  writer = csv.DictWriter(csv_file, fieldnames=fields)
+
+  writer.writeheader()
+    #writer.writerow({'이름':'짱구','수학':90,'영어':100})
+  for item in z:
+    writer.writerow(item)
+
+#dict를 json으로 만들어보기
+
+zzang = {'이름':'짱구','수학':90,'영어':100}
+import json
+with open('practice2.csv', 'w') as json_file:
+  for item in z:
+    json.dump(item, json_file)
